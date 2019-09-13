2c4487fd737619297a199d4f3ee973267d4e6709
acarlie <ameliacarlie@gmail.com>
2019-09-13 16:10:47 -0400

    Consolidated index.js into game obj, moved inquirer methods to questions.js

--- index.js
+++ index.js
@@ -1,104 +1,81 @@
 "use strict";
 
-const inquirer = require("inquirer");
 const Word = require("./word");
+const questions = require("./questions");
 
-const wordArray = ["hello", "world", "test"];
-const wordObjs = wordArray.map(x => new Word(x));
+const game = {
+    wordArray: ["hello", "world", "test"],
+    init: function(){
+        let wordObjs = this.wordArray.map(x => new Word(x));
+        let word = this.chooseWord(wordObjs);
+        let remain = this.remaining(word, wordObjs);
 
-function init(){
-    let vals = chooseWord(wordObjs);
-    let word = vals.word;
-    let remain = vals.remaining;
-    word.print();
-    guessAndRespond(word, remain);
-}
-
-function guess(){
-    return inquirer
-    .prompt({
-        type: "input",
-        message: "Guess a letter!",
-        name: "guess"
-      })
-      .then(res => { return res });
-}
- 
-function confirmPlay(){
-    return inquirer
-    .prompt({
-        type: "confirm",
-        message: "Play Again?",
-        name: "play",
-        default: true
-    })
-    .then(res => { return res });
-}
-
-function chooseWord(arr){
-    let num = Math.floor(Math.random()*arr.length);
-    return { word: arr[num], remaining: arr.filter(x => x !== arr[num]) };
-}
-
-
-
-async function playAgain(rem){
-    let res = await confirmPlay();
-
-    if (res.play){
-        let vals = chooseWord(rem);
-        let word = vals.word;
-        let remain = vals.remaining;
-        word.print();
-        guessAndRespond(word, remain);
-    } else {
-        console.log('Okay :-(');
-    }
-}
-
-
-const guessAndRespond = async (word, rem) => {
-    let res = await guess();
-    let regex = /^[a-z]$/i;
-
-    if (regex.test(res.guess)){
-
-        if (word.arr.indexOf(res.guess) === -1){
-            word.guesses--;
-            console.log(`${res.guess} was not found, you have ${word.guesses} remaining.\n`)
-        } 
-
-        word.checkLetter(res.guess);
         word.print();
 
-        checkWinOrLose(word.hasWon(), word.guesses, word, rem, guessAndRespond);
-
-        nextPuzzle(word.hasWon(), word.hasLost(), rem, playAgain)
-
-    } else {
-        console.log("Please only guess letters, one at a time. Please guess again:\n");
-        guessAndRespond(word, rem);
-    }
+        this.guessAndRespond(word, remain);
+    }, 
+    remaining: function(word, arr){
+        return arr.filter(x => x !== word);
+    },
+    chooseWord: function(arr){
+        return arr[Math.floor(Math.random()*arr.length)];
+    },
+    nextPuzzle: function(win, lose, rem, cb){
+        if ((win || lose) && rem.length > 0){
+            cb(rem);
+        } else if ((win || lose) && rem.length === 0){
+            console.log('All puzzles have been played');
+        }
     
-}
+    },
+    checkWinOrLose: function(hasWon, guesses, word, rem, cb){
+        if (!hasWon && guesses > 0){
+            cb(word, rem);
+        } else if (!hasWon && guesses === 0){
+            console.log('Youve lost :(');
+        } else {
+            console.log('Youve won!');
+        }
+    },
+    guessAndRespond: async function(word, rem){
+        let me = game;
+        let res = await questions.guess();
+        let regex = /^[a-z]$/i;
+    
+        if (regex.test(res.guess)){
+    
+            if (word.arr.indexOf(res.guess) === -1){
+                word.guesses--;
+                console.log(`${res.guess} was not found, you have ${word.guesses} remaining.\n`)
+            } 
+    
+            word.checkLetter(res.guess);
+            word.print();
+    
+            me.checkWinOrLose(word.hasWon(), word.guesses, word, rem, me.guessAndRespond);
+            me.nextPuzzle(word.hasWon(), word.hasLost(), rem, me.playAgain)
+    
+        } else {
 
-function nextPuzzle(win, lose, rem, cb){
-    if ((win || lose) && rem.length > 0){
-        cb(rem);
-    } else if ((win || lose) && rem.length === 0){
-        console.log('All puzzles have been played');
+            console.log("Please only guess letters, one at a time. Please guess again:\n");
+            me.guessAndRespond(word, rem);
+
+        }
+    },
+    playAgain: async function(rem){
+        let me = game;
+        let res = await questions.confirmPlay();
+    
+        if (res.play){
+            let word = me.chooseWord(rem);
+            let remain = me.remaining(word, rem);
+            word.print();
+            me.guessAndRespond(word, remain);
+            
+        } else {
+            console.log('Okay :-(');
+        }
     }
-
 }
 
-function checkWinOrLose(hasWon, guesses, word, rem, cb){
-    if (!hasWon && guesses > 0){
-        cb(word, rem);
-    } else if (!hasWon && guesses === 0){
-        console.log('Youve lost :(');
-    } else {
-        console.log('Youve won!');
-    }
-}
-
-init();
+game.init();
---
+++ questions.js
@@ -0,0 +1,25 @@
+const inquirer = require("inquirer");
+
+const questions = {
+    guess: function(){
+        return inquirer
+        .prompt({
+            type: "input",
+            message: "Guess a letter!",
+            name: "guess"
+        })
+        .then(res => { return res });
+    },
+    confirmPlay: function(){
+        return inquirer
+        .prompt({
+            type: "confirm",
+            message: "Play Again?",
+            name: "play",
+            default: true
+        })
+        .then(res => { return res });
+    }
+}
+
+module.exports = questions;
